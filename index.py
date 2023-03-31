a4433fa142dc17236594d50890dbcc70c03a63ba
SenchaPencha <SenchaPencha1@gmail.com>
2023-04-01 00:34:35 +0300

    Working prototype

--- index.py
+++ index.py
@@ -1,73 +1,42 @@
-import json
-from functools import lru_cache
-from deep_foundation.hasura.client import generate_apollo_client
-from deep_foundation.deeplinks.client import DeepClient, parse_jwt
+import ast
+import io
+import sys
+
 from flask import Flask, jsonify, request
 
-GQL_URN = os.environ.get("GQL_URN", "localhost:3006/gql")
-GQL_SSL = os.environ.get("GQL_SSL", 0)
-
-def to_json(data):
-    return json.dumps(data, default=lambda x: x.__dict__, indent=2)
-
-@lru_cache()
-def make_function(code):
-    fn = eval(code)
-    if not callable(fn):
-        raise ValueError("Executed handler's code didn't return a function.")
-    return fn
-
-def make_deep_client(token):
-    if not token:
-        raise ValueError("No token provided")
-    decoded = parse_jwt(token)
-    link_id = decoded.user_id
-    apollo_client = generate_apollo_client(
-        path=GQL_URN, ssl=bool(int(GQL_SSL)), token=token
-    )
-    deep_client = DeepClient(apollo_client=apollo_client, link_id=link_id, token=token)
-    return deep_client
-
 app = Flask(__name__)
+
 
 @app.route('/healthz', methods=['GET'])
 def healthz():
     return jsonify({})
 
+
 @app.route('/init', methods=['POST'])
 def init():
     return jsonify({})
 
+
 @app.route('/call', methods=['POST'])
 def call():
-    try:
-        body = request.json
-        params = body['params']
-        jwt = params['jwt']
-        code = params['code']
-        data = params['data']
-        fn = makeFunction(code)
-        deep = makeDeepClient(jwt)
-        result = await fn({ data, deep, gql, require })
-        return jsonify({ resolved: result })
-    except rejected:
-        processedRejection = JSON.parse(toJSON(rejected))
-        return jsonify({ rejected: processedRejection })
+    body = request.json
+    params = body['params']
+    data = params['data']
+    locals().update(data)
+    start = "def fn():\n" \
+            f" globals().update({data})\n"
+    code = params['code']
+    code = '\n'.join([' ' + line for line in code.splitlines()])
+    end = f"\nresult = fn()" \
+          f"\nprint(result)"
+    output = io.StringIO()
+    sys.stdout = output
+    exec(start + code + end)
+    result = output.getvalue()
+    obj = ast.literal_eval(result)
+    print(obj)
+    return jsonify(obj)
 
-@app.route('/http-call', methods=['POST'])
-def http_call():
-    try:
-        options = request.headers['deep-call-options']
-        jwt = options['jwt']
-        code = options['code']
-        data = options['data']
-        fn = makeFunction(code)
-        deep = makeDeepClient(jwt)
-        await fn(request, response, next, { data, deep, gql, require })
-    except rejected:
-        processedRejection = JSON.parse(toJSON(rejected))
-        return jsonify({ rejected: processedRejection })
 
-if __name__ == "__main__":
-    app.run(port=process.env.PORT)
-    print(f"Listening on port {process.env.PORT}")
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
