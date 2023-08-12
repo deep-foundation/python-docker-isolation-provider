a9bcb46e97b34ec02505da1ae93bb5c33d730ce6
SenchaPencha <SenchaPencha1@gmail.com>
2023-08-12 16:32:55 +0300

    more simplification

--- index.py
+++ index.py
@@ -1,36 +1,25 @@
 import asyncio
 import os
-
+import traceback
+from dotenv import load_dotenv
 from deepclient import DeepClientOptions, DeepClient
 from flask import Flask, jsonify, request
-from gql import Client
+from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 
 app = Flask(__name__)
-
-GQL_URN = os.environ.get("GQL_URN", "3006-deepfoundation-dev-adxmoff7bpv.ws-eu103.gitpod.io/gql")
+load_dotenv()
+GQL_URN = os.environ.get("GQL_URN", "localhost:3006/gql")
 GQL_SSL = os.environ.get("GQL_SSL", 0)
 TEMPLATE_CODE = """
-async def fn(arg):
-    data = arg['data']
-    # Place your logic here
 {{USER_CODE}}
-
-global result;
-result = asyncio.run(fn({{params}}))
+python_handler_context['result'] = asyncio.run(fn(python_handler_context['args']))
 """
-async def execute_handler(code, args):
-    python_handler_context = {'args': args}
-    generated_code = f"{code}\npython_handler_context['result'] = fn(python_handler_context['args'])"
-    code_object = compile(generated_code, 'python_handler', 'exec')
-    exec(code_object, dict(python_handler_context=python_handler_context))
-    result = python_handler_context['result']
-    return result
 
 def make_deep_client(token):
     if not token:
         raise ValueError("No token provided")
-    url = "https://3006-deepfoundation-dev-adxmoff7bpv.ws-eu103.gitpod.io/gql"
+    url = f"https://{GQL_URN}" if bool(int(GQL_SSL)) else f"http://{GQL_URN}"
     transport = AIOHTTPTransport(url=url, headers={'Authorization': f"Bearer {token}"})
     client = Client(transport=transport, fetch_schema_from_transport=True)
     options = DeepClientOptions(gql_client=client)
@@ -47,15 +36,21 @@ def init():
 
 @app.route('/call', methods=['POST'])
 def call():
-    body = request.json
-    params = body['params']
-    user_code = '\n'.join(['    ' + line for line in params['code'].splitlines()])
-    full_code = TEMPLATE_CODE.replace("{{USER_CODE}}", user_code).replace("{{params}}", str(params))
-    global deep;
-    deep = make_deep_client(params['jwt'])
-    exec(full_code)
-    result = globals()['result']
-    return jsonify({'resolved': result})
+    try:
+        body = request.json
+        params = body['params']
+        full_code = TEMPLATE_CODE.replace("{{USER_CODE}}", params['code'])
+        args = {
+            'deep': make_deep_client(params['jwt']),
+            'data': params['data'],
+        }
+        python_handler_context = {'args': args}
+        code_object = compile(full_code, 'python_handler', 'exec')
+        exec(code_object, globals(), dict(python_handler_context=python_handler_context))
+        result = python_handler_context['result']
+        return jsonify({'resolved': result})
+    except Exception as e:
+        return jsonify({'rejected': traceback.format_exc()})
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=os.environ.get("PORT"), use_reloader=False, threaded=True)
