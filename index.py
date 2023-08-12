b033df916d99653a06f674d0d3f7abc5cf0d29a1
SenchaPencha <SenchaPencha1@gmail.com>
2023-08-12 06:04:16 +0300

    simplification

--- index.py
+++ index.py
@@ -13,22 +13,21 @@ GQL_SSL = os.environ.get("GQL_SSL", 0)
 TEMPLATE_CODE = """
 async def fn(arg):
     data = arg['data']
-    deep = await make_deep_client(arg['jwt'])
     # Place your logic here
 {{USER_CODE}}
 
 global result;
-result = loop.create_task(fn({{params}}))
+result = asyncio.run(fn({{params}}))
 """
 async def execute_handler(code, args):
-    python_handler_context = { 'args': args }
+    python_handler_context = {'args': args}
     generated_code = f"{code}\npython_handler_context['result'] = fn(python_handler_context['args'])"
     code_object = compile(generated_code, 'python_handler', 'exec')
     exec(code_object, dict(python_handler_context=python_handler_context))
     result = python_handler_context['result']
     return result
 
-async def make_deep_client(token):
+def make_deep_client(token):
     if not token:
         raise ValueError("No token provided")
     url = "https://3006-deepfoundation-dev-adxmoff7bpv.ws-eu103.gitpod.io/gql"
@@ -47,21 +46,16 @@ def init():
     return jsonify({})
 
 @app.route('/call', methods=['POST'])
-async def call():
+def call():
     body = request.json
     params = body['params']
-    user_code = '\n'.join([' ' + line for line in params['code'].splitlines()])
-
-    # Construct full code
+    user_code = '\n'.join(['    ' + line for line in params['code'].splitlines()])
     full_code = TEMPLATE_CODE.replace("{{USER_CODE}}", user_code).replace("{{params}}", str(params))
-
-    # Execute the code
-    local_vars = {}
-    loop = asyncio.get_running_loop()
-    exec(full_code, globals(), locals())
-
-    result = await globals()['result']
-    return jsonify(result)
+    global deep;
+    deep = make_deep_client(params['jwt'])
+    exec(full_code)
+    result = globals()['result']
+    return jsonify({'resolved': result})
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=os.environ.get("PORT"), use_reloader=False, threaded=True)
